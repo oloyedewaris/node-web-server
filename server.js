9996641ecd4e88660ab59edaaf9dba9c994ee4b9
Oloyede Waris <56000914+oloyedewaris@users.noreply.github.com>
2020-04-03 14:42:34 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,44 @@
+const express= require('express');
+const hbs= require('hbs');
+const fs= require('fs');
+
+const app= express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getCurrentYear', ()=> {
+  return new Date().getFullYear();
+})
+hbs.registerHelper('upper', (text)=> {
+  return text.toUpperCase();
+});
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next)=> {
+  var now= new Date().toString();
+  var log= `${now}: ${req.method}, ${req.url}`;
+  fs.appendFileSync('server.log', log +'\n');
+  console.log(log);
+  next();
+});
+
+// app.use((req, res, next)=> {
+//   res.render('maintenance.hbs')
+// })
+
+app.get('/', (req, res)=> {
+  res.render('home.hbs', {
+    pageTitle: 'home page',
+    welcomeMessage: 'welcome to my website'
+  });
+});
+
+app.get('/about', (req, res)=> {
+  res.render('about.hbs', {
+    pageTitle: 'about page',
+  });
+});
+
+app.listen(3000, ()=> {
+  console.log('server is up on port 3000');
+});
